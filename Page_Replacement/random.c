cf31605725a03d22795c28257237b71bd6383019
Amal-nellanhi <amalnellanhi@gmail.com>
2026-05-21 16:06:53 +0530

    Add RANDOM page replacement policy

---
+++ Page_Replacement/random.c
@@ -0,0 +1,56 @@
+# include <stdio.h>
+# include <time.h>
+# include <stdlib.h>
+int main(){
+    int page , frame , index =0 , found,hit = 0 , miss = 0;
+    srand(time(0));
+    printf("Enter the number of pages : ");
+    scanf("%d",&page);
+    printf("Enter the number of frames : ");
+    scanf("%d",&frame);
+    int frames[frame];
+    for(int i = 0 ; i < frame ; i++){
+        frames[i] = -1;
+    }
+    int pages[page];
+    printf("Enter the reference string : \n");
+    for(int i = 0 ; i < page ; i++){
+        scanf("%d",&pages[i]);
+    }
+    printf("Reference\t Frames\t\tMiss/Hit\n");
+    for(int i = 0 ; i < page ; i++){
+        found = 0;
+        for(int j = 0 ; j < frame ; j++){
+            if(frames[j] == pages[i]){
+                found = 1;
+                hit ++;
+                break;
+            }
+        }
+        if(found == 0){
+            int random_index = rand() % frame;
+            frames[random_index] = pages[i];
+            miss ++; 
+        }
+        printf("%d\t\t",pages[i]);
+        for(int j = 0 ; j < frame ; j ++){
+            if(frames[j] == -1)
+                printf(" - ");
+            else
+                printf(" %d ",frames[j]);
+        }
+        if(found)
+            printf("\tHit");
+        else
+            printf("\tMiss");
+        printf("\n");
+    }
+
+    printf("Number of page miss = %d\n",miss);
+    printf("Number of page hit = %d\n",hit);
+    printf("Hit rate = %.2f\n",(float)hit / page);
+    printf("Miss rate = %.2f\n",(float)miss / page);
+
+    return 0;
+    
+}
